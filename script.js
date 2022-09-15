3ad6e5e62aa344a1e276efe53b30aa8328c0eff6
Martinitu <martinitu@gmail.com>
2022-09-14 21:24:34 -0500

    First java and basic html added

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+var add = function(a, b) {
+return a + b;
+}
+var subtract = function(a, b) {
+    return a - b;
+    }
+var multiply = function(a, b) {
+    return a * b;
+    }
+var divide = function(a, b) {
+        return a / b;
+        }
 
+var operate = function (op, a, b){
+    return op(a, b)
+}
