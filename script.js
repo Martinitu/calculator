2581d26ebf84ea2ba600f4563d27c7c8922e481e
Martinitu <martinitu@gmail.com>
2022-09-20 18:54:41 -0500

    function for subtract add nad multiply and divide added and tested

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var add = function(a, b) {
-return a + b;
+return a += b;
 }
 var subtract = function(a, b) {
     return a - b;
@@ -11,6 +11,146 @@ var divide = function(a, b) {
         return a / b;
         }
 
-var operate = function (op, a, b){
-    return op(a, b)
+var operate = function(op, a, b) {
+    return   op(a, b)
+    
+     
+
 }
+
+
+let display = document.getElementById("display")
+let displayValue = 0;
+let firstValue = 0;
+let secondValue = 0
+let operator 
+let result
+
+let buttons = Array.from(document.getElementsByClassName("button"));
+
+let equals = function() {
+    
+}
+
+buttons.map( button => {
+    button.addEventListener('click', (e) => {
+        switch(e.target.innerText) {
+            case "AC": display.innerText = "";
+                       displayValue = display.innerText;;
+                       firstValue = 0;
+                       secondValue = 0;
+                       displayValue = 0;
+            break
+            case "=": 
+                    secondValue = displayValue;
+                    result = operate(operator, firstValue, secondValue)
+                    display.innerText = result;
+                    firstValue = result
+                    secondValue = 0;
+                   
+                    displayValue = 0;
+                    
+                    console.log(firstValue, secondValue, displayValue, operator)
+            break
+            case "+": 
+                        
+                    if (firstValue == 0) {
+                        operator = add
+                        firstValue = displayValue;
+                        displayValue = 0;
+                        display.innerText = "";
+                    
+                    } else  {
+                        operator = add
+                        secondValue = displayValue;
+                        
+                        result = operate(operator, firstValue, secondValue)
+                        display.innerText = result;
+                        firstValue = result
+                        secondValue = 0;
+                        displayValue = 0
+                       
+                    } 
+                    console.log(firstValue, secondValue, displayValue, operator)
+            break
+            case "-": 
+                        
+            if (firstValue == 0) {
+                operator = subtract
+                firstValue = displayValue;
+                displayValue = 0;
+                display.innerText = "";
+            
+            } else  {
+                operator = subtract
+                secondValue = displayValue;
+                
+                result = operate(operator, firstValue, secondValue)
+                display.innerText = result;
+                firstValue = result
+                secondValue = 0;
+                displayValue = 0
+               
+            } 
+            console.log(firstValue, secondValue, displayValue, operator)
+    break
+    case "X": 
+                        
+    if (firstValue == 0) {
+        operator = multiply
+        firstValue = displayValue;
+        displayValue = 0;
+        display.innerText = "";
+    
+    } else if (secondValue == 0 && displayValue != 0) {
+        operator = multiply
+        secondValue = displayValue;
+        
+        result = operate(operator, firstValue, secondValue)
+        display.innerText = result;
+        firstValue = result
+        secondValue = 0;
+        displayValue = 0
+       
+    } 
+    console.log(firstValue, secondValue, displayValue, operator)
+break
+   
+
+    case "/": 
+                        
+    if (firstValue == 0) {
+        operator = divide
+        firstValue = displayValue;
+        displayValue = 0;
+        display.innerText = "";
+    
+    } else if (secondValue == 0 && displayValue != 0) {
+        operator = divide
+        secondValue = displayValue;
+        
+        result = operate(operator, firstValue, secondValue)
+        display.innerText = result;
+        firstValue = result
+        secondValue = 0;
+        displayValue = 0
+       
+    } 
+    console.log(firstValue, secondValue, displayValue, operator)
+break
+            default: 
+                    if (displayValue == 0 && display.innerText != "" ) {
+                        display.innerText = "";
+                        display.innerText += e.target.innerText;
+                        displayValue = parseInt(display.innerText);
+
+                    } else {
+                        display.innerText += e.target.innerText;
+                        displayValue = parseInt(display.innerText);
+                  
+                    }
+                    console.log(firstValue, secondValue, displayValue)
+                  
+        }
+    } )
+})
